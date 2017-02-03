0a1bb2d596bd5c847ccbe2387920a98a4f4cef8c
Simonet <Simonet@Aula-B4-PC-32>
2017-02-03 11:49:27 +0000

    1

---
+++ src/summa.java
@@ -0,0 +1,12 @@
+
+public class summa {
+			public static void main(String[] args) {
+			int nota1=3;
+			int nota2=10;
+			int nota3=6;
+			double media;
+			
+			media = (nota1 + nota2 + nota3) / 3;
+			System.out.println("La nota media es: " + media);
+		}
+}
